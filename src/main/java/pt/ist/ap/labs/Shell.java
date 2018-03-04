e29edf13f253263f316d0596bf555359ea5236de
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2018-03-04 19:24:16 +0000

    Add base configurations

--- src/main/java/pt/ist/ap/labs/Shell.java
+++ src/main/java/pt/ist/ap/labs/Shell.java
@@ -1,7 +1,25 @@
 package pt.ist.ap.labs;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Shell {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the Simple Introspection Shell!");
+
+        while (in.hasNext()) {
+            System.out.print("Command:> ");
+            System.out.flush();
+            String command = in.nextLine();
+            String[] splitCommand = command.split(" ");
+            String[] arguments = Arrays.copyOfRange(splitCommand, 1, splitCommand.length);
+            System.out.println("Command: " + command);
+            System.out.println("Arguments:");
+            for (String arg : arguments) {
+                System.out.print(" - ");
+                System.out.println(arg);
+            }
+        }
     }
 }
