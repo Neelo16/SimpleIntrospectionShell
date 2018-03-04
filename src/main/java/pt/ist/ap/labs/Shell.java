0a7525d55d165d54c02110436d02d00e89288274
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2018-03-04 19:45:04 +0000

    Change scanner to BufferedReader

--- src/main/java/pt/ist/ap/labs/Shell.java
+++ src/main/java/pt/ist/ap/labs/Shell.java
@@ -1,25 +1,21 @@
 package pt.ist.ap.labs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Shell {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        String command;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Welcome to the Simple Introspection Shell!");
 
-        while (in.hasNext()) {
+        while ((command = in.readLine()) != null) {
             System.out.print("Command:> ");
             System.out.flush();
-            String command = in.nextLine();
             String[] splitCommand = command.split(" ");
             String[] arguments = Arrays.copyOfRange(splitCommand, 1, splitCommand.length);
-            System.out.println("Command: " + command);
-            System.out.println("Arguments:");
-            for (String arg : arguments) {
-                System.out.print(" - ");
-                System.out.println(arg);
-            }
         }
     }
 }
