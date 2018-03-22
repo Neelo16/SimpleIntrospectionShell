3cb62b0215de232333e471eb55915badd41d5dfe
Tomás Cunha <tomas.cunha@tecnico.ulisboa.pt>
2018-03-22 12:27:43 +0000

    Add skeleton for complex object parsing

--- src/main/java/pt/ist/ap/labs/ArgumentParser.java
+++ src/main/java/pt/ist/ap/labs/ArgumentParser.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class ArgumentParser {
 
+    private String[] arguments;
+    private int argumentIndex;
     private ArrayList<Object> parsedObjects = new ArrayList<>();
 
     public ArgumentParser(String[] arguments) {
@@ -11,7 +13,35 @@ public class ArgumentParser {
     }
 
     public Object[] parse() {
+        try {
+            for (argumentIndex = 0; argumentIndex < arguments.length; argumentIndex++) {
+                String argument = arguments[argumentIndex];
+                Object instance;
+                if (isNew(argument)) {
+                    instance = instantiateObject();
+                } else {
+                    throw new RuntimeException("Failed to parse arguments");
+                }
+                parsedObjects.add(instance);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Failed to parse arguments", e);
+        }
         return parsedObjects.toArray();
     }
 
+    private boolean isNew(String argument) {
+        return argument.equals("new");
+    }
+
+    // TODO
+    private Object instantiateObject() {
+        String argument = arguments[++argumentIndex];
+        int arrayDepth = 0;
+        while (argument.matches(".*\\[]$")) {
+            ++arrayDepth;
+            argument = argument.substring(0, argument.length() - 2);
+        }
+        return null;
+    }
 }
